94003581017f4cfae85a0facaf0f2f22551210eb
djacobi19 <deepakjacobi@yahoo.com>
2025-01-15 15:52:26 -0500

    Add files via upload

---
+++ PDFTK/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GRAY AREA RETIREMENT</title>
+    <!-- Below is bootstrap link for styling. It is a framework with predefined style functions -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js">
+    <link rel="stylesheet" href="style.css">
+    <!-- above is bootstrap link for styling. It is a framework with predefined style functions -->
+    
+</head>
+<body>
+ 
+    
+       
+          
+             
+                
+                   
+                      
+                         
+                            
+                               
+                                  
+                                     
+                                        
+                                           
+                                              
+                                                 
+                                                    
+                                                       
+                                                          
+                                                                
+</body>
+</html>
